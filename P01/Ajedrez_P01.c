1f1b4c4371bebd689e20a2069f6610b861b75fb1
Ixchel Urzúa <516urzuacruzmonicaixchel@gmail.com>
2023-03-01 22:52:48 -0600

    Correccion de ajedrez Final

---
+++ P01/Ajedrez_P01.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Variables globales
+int i,j,x,y;
+
+//Menu
+int pedirOpcion(int opMax)
+{
+    int op;
+    char opChar[10];
+    
+    fflush(stdin);
+    do                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    {
+        printf("-> "); gets(opChar);
+        op = atoi(opChar);
+        if(op < 1)
+        printf("Opcion invalida\n");
+        else if(op > opMax)
+        printf("Opcion invalida. El maximo es %d\n", opMax);
+        printf("\n");
+    } while (op<1 || op > opMax);
+    fflush(stdin);
+
+    return op;
+}
+int menuPrincipal()
+{
+    system("cls");
+    printf("*******MENU PRINCIPAL*******\n");
+    printf(".:Simulador de movimiento para la torre y el alfil:.\n");
+    printf("1)Simulador del alfil\n");
+    printf("2)Simulador de la torre\n");
+    printf("3)Salir\n\n");
+
+    printf(".:Seleccione una opcion:.\n");
+    return pedirOpcion(3);
+}
+int menuRetorno()
+{
+printf("*******MENU DE RETORNO*******\n");
+printf("1)Repetir simulacion\n");
+printf("2)Volver al menu principal\n");
+printf("3)Salir\n\n");
+
+printf(".:Seleccione una opcion:.\n");
+return pedirOpcion(3);
+}
+void movimientoAlfil()
+{
+int tablero[9][9];
+printf("\nIngrese la posicion del alfil en este formato -> x,y:"); 
+    scanf("%d,%d",&x,&y); 
+
+// crerar toda la matriz 
+
+    for (i=1; i<9; i++){
+        for (j=1; j<9; j++){ 
+            tablero[i][j]=0; 
+        }
+    }
+      
+//inferior izquierda
+for ( i=x, j=y; (j<9&&i>=1); i--, j++) {
+    tablero[i][j]=1;                
+}
+
+//inferior derecha
+for (i=x, j=y; (i>=1&&j<9); i++, j++){
+    tablero[i][j]=1; 
+}
+
+//superior derecha
+for (i=x, j=y; (i>1&&j>=1); i++, j--){
+    tablero[i][j]=1; 
+}
+
+//superior izquierda
+for (i=x, j=y; (i>=1&&j>=1); i--, j--){
+    tablero[i][j]=1; 
+}
+
+                     
+
+    tablero[x][y]=8; // la posicion del alfil 
+    
+    
+    printf("El alfil esta en %d,%d\n",x,y);  
+    for (j=1; j<9; j++){
+        for (i=1; i<9; i++){ 
+                if(tablero[i][j]==0){
+                    printf("*\t"); 
+                } 
+                if(tablero[i][j]==1){
+                    printf("P\t");
+                } 
+                if(tablero[i][j]==8){
+                    printf("A\t");
+                } 
+        }
+            printf("\n\n"); 
+    }  
+}
+void movimientoTorre()
+{
+int tablero[9][9];
+printf("\nIngrese la posicion de la torre en este formato -> x,y:"); 
+    scanf("%d,%d",&x,&y); 
+
+// crerar toda la matriz 
+
+    for (i=1; i<9; i++){
+        for (j=1; j<9; j++){ 
+            tablero[i][j]=0; 
+        }
+    }
+
+// Arriba
+for ( i=x, j=y; (i<9&&j>=0);j-- ) {
+    tablero[i][j]=1;                
+}   
+//Derecha 
+for ( i=x, j=y; (i<9&&j>=0);i++ ) {
+   tablero[i][j]=1;                
+}     
+//izquierda             
+for (i=x, j=y; (i>0&&j>=1); i--){
+    tablero[i][j]=1; 
+}  
+
+//Abajo
+for ( i=x, j=y; (i>0&&j<9);j++ ) {
+    tablero[i][j]=1;  
+}
+
+    tablero[x][y]=8; // la posicion de la torre 
+    
+    
+    printf("La torre esta en %d,%d\n",x,y);  
+    for (j=1; j<9; j++){
+        for (i=1; i<9; i++){ 
+                if(tablero[i][j]==0){
+                    printf("*\t"); 
+                } 
+                if(tablero[i][j]==1){
+                    printf("P\t");
+                } 
+                if(tablero[i][j]==8){
+                    printf("T\t");
+                } 
+        }
+            printf("\n\n"); 
+    }  
+}
+
+int main()
+{
+	int opcion;
+	
+	inicio:
+		opcion= menuPrincipal();
+		
+		seleccion:
+		system("cls");
+		switch (opcion)
+		{
+			case 1: movimientoAlfil(); break;
+			case 2: movimientoTorre();break;
+			default: goto salir; break;
+		}
+		
+		switch (menuRetorno())
+		{
+			case 1: goto seleccion; break;
+			case 2: goto inicio; break;
+			default: break;
+		}
+		salir:
+			printf("\nGracias por usar el programa :)\n\n");
+			
+		return 0;
+}
