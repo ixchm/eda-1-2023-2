a4dffb93ff8b210b35063fae42dcf843fdfa7165
Ixchel Urzua <516urzuacruzmonicaixchel@gmail.com>
2023-03-03 14:41:01 +0000

    Ejemolos

---
+++ EjemplosDeLaboratorio/ApuntadorArreglo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+*/
+int main () {
+short arr[5], *apArr;
+apArr = &arr[0];
+// imprime la dirección de memoria del arreglo en la posición [0]
+printf("Dirección del arreglo en la primera posición: %x\n",&arr[0]);
+// imprime la dirección de memoria del arreglo
+// (el nombre del arreglo es un apuntador)
+printf("Dirección del arreglo: %x\n",&arr);
+// imprime la dirección de memoria del apuntador apArr
+printf("Dirección del apuntador: %x\n",apArr);
+}
+//Este programa crea un apuntador de tipo entero
+//que apunta al inicio de un arreglo.
+return 0;
+ 
